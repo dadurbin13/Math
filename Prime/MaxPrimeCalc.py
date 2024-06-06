e8482d26d20bda32961e08d65eac369459cb3717
Gleipner <dadurbin13@pm.me>
2024-06-06 15:53:19 -0600

    MaxPrimeCalc
    Complete with Log

---
+++ Prime/MaxPrimeCalc.py
@@ -0,0 +1,45 @@
+import signal
+from datetime import datetime
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def load_last_number(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        if lines:
+            last_line = lines[-1].strip().split('- ')[-1]
+            return int(last_line)
+        return 0
+
+def signal_handler(sig, frame):
+    global max_prime
+    with open("MaxPrime.log", "w") as file:
+        file.write(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {max_prime}")
+        print(f"\n{datetime.now():%Y-%m-%d %H:%M:%S} - {max_prime:,}")
+    print("Exiting...")
+    exit(0)
+
+def main():
+    global max_prime
+    filename = "MaxPrime.log"
+
+    num = load_last_number(filename)
+    max_prime = num
+
+    while True:
+        num += 1
+        if is_prime(num):
+            max_prime = num
+        
+        if num % 1000000 == 0:
+            print(f"{datetime.now():%Y-%m-%d %H:%M:%S} - {max_prime:,}")
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
