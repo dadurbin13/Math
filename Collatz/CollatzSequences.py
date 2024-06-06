9d520aede4a2f2342aa1dd04507f0d93553a2d12
Gleipner <dadurbin13@pm.me>
2024-06-05 18:43:28 -0600

    Collatz Sequences
    it now prints and saves the sequences

--- Collatz/CollatzSequences.py
+++ Collatz/CollatzSequences.py
@@ -1,9 +1,6 @@
-import signal
-stop_requested = False
-
 def collatz_sequence(n):
     sequence = [n]
-    while n > 1:
+    while n != 1:
         if n % 2 == 0:
             n = n // 2
         else:
@@ -11,39 +8,14 @@ def collatz_sequence(n):
         sequence.append(n)
     return sequence
 
-def update_log_file(log_file, sequence):
-    with open(log_file, 'a') as file:
-        file.write(','.join(map(str, sequence)) + '\n')
+def save_collatz_sequences(sequence, filename):
+    with open(filename, 'a') as file:
+        file.write(f"{', '.join(str(num) for num in sequence)}\n")
 
-def signal_handler(signum, frame, log_file):
-    print("\nInterrupt received, stopping...")
-    update_log_file(log_file, sequence)
-    global stop_requested
-    stop_requested = True
-
-def find_special_collatz_numbers(log_file="CollatzSequences.log"):
-    global stop_requested
-    stop_requested = False
-
-    signal.signal(signal.SIGINT, lambda s, f: signal_handler(s, f, log_file))
-
-    with open(log_file, 'r') as file:
-        lines = file.readlines()
-        if lines:
-            n_str = ','.join(lines[-1].strip().split(',')[:-1])
-            n = int(n_str)
-        else:
-            n = 1
-        print(f"Continuing from {n}")
-
-    sequence = collatz_sequence(n)
-
-    while not stop_requested:
-        update_log_file(log_file, sequence)
-        print(','.join(map(str, sequence)))
-        sequence = collatz_sequence(sequence[-1])
-
-    update_log_file(log_file, sequence)
-    print(f"Finished at {sequence[0],}")
-
-find_special_collatz_numbers()
+sequences = []
+num = 1
+while num < 11:
+    sequence = collatz_sequence(num)
+    save_collatz_sequences(sequence, 'CollatzSequences.log')
+    print(f"{sequence}")
+    num += 1
