c1c3dc88cb0e39918f40920d6a161a0e7b9bacb2
Gleipner <dadurbin13@pm.me>
2024-06-05 19:54:29 -0600

    Sequences
    It will now find every unique sequence up to the given number.

--- Collatz/CollatzSequences.py
+++ Collatz/CollatzSequences.py
@@ -8,14 +8,52 @@ def collatz_sequence(n):
         sequence.append(n)
     return sequence
 
+def load_last_number(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        if lines:
+            last_line = lines[-1].strip().split(', ')[0]
+            return int(last_line)
+        return 0
+
 def save_collatz_sequences(sequence, filename):
     with open(filename, 'a') as file:
         file.write(f"{', '.join(str(num) for num in sequence)}\n")
 
-sequences = []
-num = 1
-while num < 11:
-    sequence = collatz_sequence(num)
-    save_collatz_sequences(sequence, 'CollatzSequences.log')
-    print(f"{sequence}")
-    num += 1
+def remove_duplicates(filename):
+    with open(filename, "r") as file:
+        log_content = file.read()
+
+    lines = log_content.split("\n")
+
+    unique_lines = []
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if not line:
+            unique_lines.append(lines[i-1].strip())
+            continue
+        for j in range(i + 1, len(lines)):
+            line2 = lines[j].strip()
+            if not line2:
+                continue
+            if not set(line).issubset(line2):
+                unique_lines.append(line)
+                break
+
+    with open(filename, "w") as file:
+        file.write("\n".join(unique_lines))
+
+def main():
+    filename = "CollatzSequences.log"
+
+    num = load_last_number(filename)
+
+    while num < 50:
+        num += 1
+        sequence = collatz_sequence(num)
+        save_collatz_sequences(sequence, filename)
+
+    remove_duplicates(filename)
+
+if __name__ == "__main__":
+    main()
